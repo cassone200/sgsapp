0aac4d8b626276136fbaff47f8a76715b4870cc0
thiago.pereira <thiago.cassone@gmail.com>
2016-10-13 14:14:50 -0300

    Correções ajax

--- Lokera/www/js/app.js
+++ Lokera/www/js/app.js
@@ -68,8 +68,11 @@ $('#listar').on('click', function() {
        
       
        
-      for (var i = 0; i < (data.length); i++) {
-            $("#conteudo").append('<div>'+ data[i].nome +'</div>');
+      for (var i = 0; i < (data.length); i++) {       
+          
+          
+          
+$("#conteudo").append('<div class=demo-card-square mdl-card mdl-shadow--2dp><div class=mdl-card__title mdl-card--expand><h2 class=mdl-card__title-text>' + data[i].nome + '</h2></div><div class=mdl-card__actions mdl-card--border><a class=mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect>View Updates</a></div></div>');
        }
        
        
